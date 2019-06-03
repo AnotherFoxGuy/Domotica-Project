15a86a2f3c720fe144439fbeed31f7ec814242ef
Edgar <edgar@anotherfoxguy.com>
2019-06-03 13:25:43 +0200

    :wrench: Replace aREST with MQTT

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,109 +1,71 @@
-/*
-  This a simple example of the aREST Library for Arduino (Uno/Mega/Due/Teensy)
-  using the Ethernet library (for example to be used with the Ethernet shield).
-  See the README file for more details.
+// This example uses an Arduino Uno together with
+// an Ethernet Shield to connect to shiftr.io.
+//
+// You can check on your device after a successful
+// connection here: https://shiftr.io/try.
+//
+// by Joël Gähwiler
+// https://github.com/256dpi/arduino-mqtt
 
-  Written in 2014 by Marco Schwartz under a GPL license.
-*/
-
-#define LIGHTWEIGHT 1
-
-// Libraries
-#include <SPI.h>
 #include <Ethernet.h>
-#include <aREST.h>
-#include <avr/wdt.h>
-#include <NewRemoteTransmitter.h>
+#include <MQTT.h>
 
-// Create a transmitter on address 123, using digital pin 11 to transmit, 
-// with a period duration of 260ms (default), repeating the transmitted
-// code 2^3=8 times.
-NewRemoteTransmitter transmitter(31787478, 11, 260, 3);
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+byte ip[] = {192, 168, 1, 3};  // <- change to match your network
 
-// Enter a MAC address for your controller below.
-byte mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0xFE, 0x40};
+EthernetClient net;
+MQTTClient client;
 
-// IP address in case DHCP fails
-IPAddress ip(192, 168, 1, 3);
+unsigned long lastMillis = 0;
 
-// Ethernet server
-EthernetServer server(80);
+void connect() {
+  Serial.print("connecting...");
+  while (!client.connect("Kaas-Knabelaar", "XXXXX", "XXXXX")) {
+    Serial.print(".");
+    delay(1000);
+  }
 
-// Create aREST instance
-aREST rest = aREST();
+  Serial.println("\nconnected!");
 
-// Variables to be exposed to the API
-int temperature;
-int humidity;
-
-// Declare functions to be exposed to the API
-int ledControl(String command);
-
-// Custom function accessible by the API
-int ledControl(String command) {
-
-    // Get state from command
-    int state = command.toInt();
-
-    digitalWrite(6, state);
-    return 1;
-
+  client.subscribe("/hello");
+  // client.unsubscribe("/hello");
 }
 
-// Custom function accessible by the API
-int transmitterTurnOnControl(String command) {
-    // Get state from command
-    transmitter.sendUnit(command.toInt(), true);
-    return 1;
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
 }
 
-
-// Custom function accessible by the API
-int transmitterTurnOffControl(String command) {
-    // Get state from command
-    transmitter.sendUnit(command.toInt(), false);
-    return 1;
-}
-
-void setup(void) {
-    // Start Serial
-    Serial.begin(9600);
-
-    // Init variables and expose them to REST API
-    temperature = 24;
-    humidity = 40;
-    rest.variable("temperature", &temperature);
-    rest.variable("humidity", &humidity);
-
-    // Function to be exposed
-    rest.function("led", ledControl);
-    rest.function("transmitterOff", transmitterTurnOnControl);
-    rest.function("transmitterOn", transmitterTurnOffControl);
-
-    // Give name & ID to the device (ID should be 6 characters long)
-    rest.set_id("3621");
-    rest.set_name("Kaas_Knabelaar");
-
-    // Start the Ethernet connection and the server
-    //if (Ethernet.begin(mac) == 0) {
+void setup() {
+  Serial.begin(9600);
+   // Start the Ethernet connection and the server
+    if (Ethernet.begin(mac) == 0) {
     // Serial.println("Failed to configure Ethernet using DHCP");
     // no point in carrying on, so do nothing forevermore:
     // try to congifure using IP address instead of DHCP:
     Ethernet.begin(mac, ip);
-    // }
-    server.begin();
+    }
+
     Serial.print("server is at ");
     Serial.println(Ethernet.localIP());
 
-    // Start watchdog
-    wdt_enable(WDTO_4S);
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+  // You need to set the IP address directly.
+  client.begin("vps693792.ovh.net", net);
+  client.onMessage(messageReceived);
+
+  connect();
 }
 
 void loop() {
+  client.loop();
 
-    // listen for incoming clients
-    EthernetClient client = server.available();
-    rest.handle(client);
-    wdt_reset();
+  if (!client.connected()) {
+    connect();
+  }
 
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    client.publish("/hello", "world");
+  }
 }
