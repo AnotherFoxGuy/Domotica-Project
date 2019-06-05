b65357c49d3d58be2dbb9d6cf15a72d9e632b40c
Roland <roland_zwart@outlook.com>
2019-06-05 12:32:58 +0200

    progress

--- App/HelloApp/HelloApp/MainPage.xaml.cs
+++ App/HelloApp/HelloApp/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Net.Mqtt;
 
 namespace HelloApp
 {
@@ -13,26 +14,36 @@ namespace HelloApp
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
-        private List<string> _names;
-        
-        
+        IMqttClient _client;
+
         public MainPage()
         {
             InitializeComponent();
-            _names = new List<string>();
+            var configuration = new MqttConfiguration();
+
+            var kaas = Task.Run(async () => await MqttClient.CreateAsync("vps693792.ovh.net", configuration));
+            kaas.Wait();
+            _client = kaas.Result;
+
+            Task.Run(async () => await _client.ConnectAsync(new MqttClientCredentials("Kaas-App", "mqtt-test", "mqtt-test"))).Wait();
         }
 
-
-        private void AddButton_OnClicked(object sender, EventArgs e)
+        private void Switch1_Toggled(object sender, ToggledEventArgs e)
         {
-            if (!string.IsNullOrEmpty(NameEntry.Text))
-                _names.Add(NameEntry.Text);
-
+            var message = new MqttApplicationMessage("Switch/Toggled", Encoding.UTF8.GetBytes("1"));
+            Task.Run(async () => await _client.PublishAsync(message, MqttQualityOfService.AtMostOnce)).Wait();
         }
 
-        private void ShowButton_OnClicked(object sender, EventArgs e)
+        private void Switch2_Toggled(object sender, ToggledEventArgs e)
         {
-            Navigation.PushAsync(new ListPage(_names));
+            var message = new MqttApplicationMessage("Switch/Toggled", Encoding.UTF8.GetBytes("2"));
+            Task.Run(async () => await _client.PublishAsync(message, MqttQualityOfService.AtMostOnce)).Wait();
+        }
+
+        private void Switch3_Toggled(object sender, ToggledEventArgs e)
+        {
+            var message = new MqttApplicationMessage("Switch/Toggled", Encoding.UTF8.GetBytes("3"));
+            Task.Run(async () => await _client.PublishAsync(message, MqttQualityOfService.AtMostOnce)).Wait();
         }
     }
 }
