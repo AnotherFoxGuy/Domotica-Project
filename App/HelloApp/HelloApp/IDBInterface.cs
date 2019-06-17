da9e8fd511d69366dfd2b8b9e2089038c47f488d
Roland <roland_zwart@outlook.com>
2019-06-17 12:38:49 +0200

    added all code from app week 4

---
+++ App/HelloApp/HelloApp/IDBInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace HelloApp
+{
+    public interface IDBInterface
+    {
+        SQLiteConnection CreateConnection();
+    }
+}
---
+++ App/HelloApp/HelloApp/Models/Serie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace HelloApp.Models
+{
+    public class Serie
+    {
+        [PrimaryKey, AutoIncrement, Unique, NotNull]
+        public int SerieID { get; set; }
+
+        [Unique, NotNull]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [NotNull]
+        public int YearStarted { get; set; }
+
+        public int YearEnded { get; set; }
+
+        [NotNull]
+        public int NumberOfEpisodes { get; set; }
+
+        [NotNull]
+        public int NumberOfSeasons { get; set; }
+    }
+}
