3230ffd931988cc2f5f0cc88b5ae7f96bcee7b99
Roland <roland_zwart@outlook.com>
2019-06-18 11:56:03 +0200

    app reset

---
+++ App/Project_Green/Project_Green/Project_Green/Models/Greenhouse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Project_Green.Models
+{
+    public class Greenhouse
+    {
+        [Unique, PrimaryKey, NotNull]
+        public string ID { get; set; }
+
+        [Unique]
+        public string Name { get; set; }
+
+        [Unique, NotNull]
+        public string IP { get; set; }
+    }
+}
---
+++ App/Project_Green/Project_Green/Project_Green/Models/Sensor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Green.Models
+{
+    public class Sensor
+    {
+        public int ID { get; set; }
+        public decimal Temperature { get; set; }
+        public decimal Humidity { get; set; }
+        public decimal Moisture { get; set; }
+        public int LightLevel { get; set; }
+        public bool WaterLevel { get; set; }
+    }
+}
