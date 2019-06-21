86c5ed014733d5fce765d38021ac86e5d5fa4550
Dante Klijn <danteklijn01@gmail.com>
2019-06-21 13:17:01 +0200

    Graph Page almoste done
    only connection with database

--- App/Project_Green/StatMeister.cs
+++ App/Project_Green/StatMeister.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microcharts;
+using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,18 +8,22 @@ namespace Project_Green
 {
     class StatMeister
     {
+        int AmountOfTimes = 0;
+        
+       
         public int Limitcheck(int max, int min, int value)
         {
-            if (value < min) { value = 0; }
-            if (value > max) { value = 100; }
+            if (value < min) { value = min; }
+            if (value > max) { value = max; }
             return value;
         }
         public string ColorRep(int value, int max , int min)
         {
             string color = "";
-            int convertedvalue = convert(value,max,min);
-            color = convertedvalue.ToString();
-            Dictionary<int, string> TimeDates = new Dictionary<int, string>();
+            long convertedvalue = convert(value,max,min);
+            
+            Dictionary<long, string> TimeDates = new Dictionary<long, string>();
+            TimeDates.Add(0, "#FF1493");
             TimeDates.Add(1, "#FF1493");
             TimeDates.Add(2, "#FF1493");
             TimeDates.Add(3, "#FF1493");
@@ -28,14 +34,207 @@ namespace Project_Green
             TimeDates.Add(8, "#FF1493");
             TimeDates.Add(9, "#FF1493");
             TimeDates.Add(10, "#FF1493");
+            switch (convertedvalue)
+            {
+                case 0:
+                    color = TimeDates[convertedvalue];
+                    break;
+                case 1:
+                    color = TimeDates[convertedvalue];
+                    break;
+                case 2:
+                    color = TimeDates[convertedvalue];
+                    break;
+                case 3:
+                    color = TimeDates[convertedvalue];
+                    break;
+                case 4:
+                    color = TimeDates[convertedvalue];
+                    break;
+                case 5:
+                    color = TimeDates[convertedvalue];
+                    break;
+                case 6:
+                    color = TimeDates[convertedvalue];
+                    break;
+                case 7:
+                    color = TimeDates[convertedvalue];
+                    break;
+                case 8:
+                    color = TimeDates[convertedvalue];
+                    break;
+                case 9:
+                    color = TimeDates[convertedvalue];
+                    break;
+                case 10:
+                    color = TimeDates[convertedvalue];
+                    break;
+
+            }
+
             return color;
         }
 
-        public int convert (int value,int max ,int min)
+        public long convert (int value,int max ,int min)
         {
-            return (value - max) / (min - max) * (10 - 0) + 10;           
+            return (value - min) * (10 - 0) / (max - min) + 0;
         }
-       
+        public int EntryCount(string dateselected)
+        {
+            Dictionary<string, int> TimeDates = new Dictionary<string, int>();
+            TimeDates.Add("Week", 7);
+            TimeDates.Add("Month", 12);
+            TimeDates.Add("Year", 24);
+
+            switch (dateselected)
+            {
+                case "Week":
+                    AmountOfTimes = TimeDates["Week"];
+                    break;
+                case "Month":
+                    AmountOfTimes = TimeDates["Month"];
+                    break;
+                case "Year":
+                    AmountOfTimes = TimeDates["Year"];
+                    break;
+                default:
+                    AmountOfTimes = TimeDates["Week"];
+                    break;
+            }
+            return AmountOfTimes;
+        }
+
+        public LineChart MakeLineChart(int dateselecter)
+        {
+            LineChart chart;
+            List<ChartEntry> entriesH = new List<ChartEntry>();
+            //dummydata
+            List<int> Week = new List<int> { 10, 20, 33, 40, 50, 60, 70, 8 };
+            List<int> Month = new List<int> { 10, 20, 33, 40, 50, 60, 70, 8, 10, 20, 33, 40 };
+            List<int> Year = new List<int> { 10, 20, 33, 40, 50, 60, 70, 8, 10, 20, 33, 40, 50, 60 , 10, 20, 33, 40, 50, 60, 70, 8, 10, 20};
+            List<int> dates = new List<int>();
+            switch(dateselecter)
+            {
+                case 7:
+                    dates = Week;
+                    break;
+                case 12:
+                    dates = Month;
+                    break;
+                case 24:
+                    dates = Year;
+                    break;
+
+            }
+      
+            foreach (int value in dates)
+            {
+                int i = Limitcheck(100, 0, value);
+                string color = ColorRep(value, 100, 0);
+                entriesH.Add(new ChartEntry(i) { Color = SKColor.Parse(color), Label = dateselecter.ToString(), ValueLabel = value.ToString() });
+            }
+            chart = new LineChart()
+            {
+                Entries = entriesH,
+                LineMode = LineMode.Straight,
+                LineSize = 8,
+                LabelTextSize = 50,
+                PointMode = PointMode.Circle,
+                PointSize = 25,
+                MaxValue = 100,
+                MinValue = 0
+            };
+            //max 100%
+            //min 0%
+            return chart;
+        }
+        public LineChart FillTempratureChart(int dateselecter)
+        {
+            LineChart chart;
+            List<ChartEntry> entriesT = new List<ChartEntry>();
+            List<int> Week = new List<int> { 10, 20, 33, 40, -20, 9, -10 };
+            List<int> Month = new List<int> { 10, 20, 33, 40, -20, 9, -10, 10, 20, 33, 40, -20 };
+            List<int> Year = new List<int> { 10, 20, 33, 40, -20, 9, -10, 10, 20, 33, 40, -20, 10, 20, 33, 40, -20, 9, -10, 10, 20, 33, 40, -20, };
+            List<int> dates = new List<int>();
+            switch (dateselecter)
+            {
+                case 7:
+                    dates = Week;
+                    break;
+                case 12:
+                    dates = Month;
+                    break;
+                case 24:
+                    dates = Year;
+                    break;
+
+            }
+           
+            foreach (var value in dates)
+            {
+
+                int i = Limitcheck(50, -50, value);
+                string color = ColorRep(value, 50, -50);
+                entriesT.Add(new ChartEntry(i) { Color = SKColor.Parse(color), Label = "maandag", ValueLabel = value.ToString() });
+            }
+            chart = new LineChart()
+            {
+                Entries = entriesT,
+                LineMode = LineMode.Straight,
+                LineSize = 8,
+                LabelTextSize = 50,
+                PointMode = PointMode.Circle,
+                PointSize = 25,
+                MaxValue = 50,
+                MinValue = -50
+            };
+            
+            return chart;
+        }
+        public LineChart FillSoilMoisterChart(int dateselecter)
+        {
+            LineChart chart;
+            List<ChartEntry> entriesS = new List<ChartEntry>();
+            List<int> Week = new List<int> { 20, 33, 40, 50, 60, 70, 8 };
+            List<int> Month = new List<int> { 20, 33, 40, 50, 60, 70, 8, 20, 33, 40, 50, 60 };
+            List<int> Year = new List<int> { 20, 33, 40, 50, 60, 70, 8, 20, 33, 40, 50, 60 , 20, 33, 40, 50, 60, 70, 8, 20, 33, 40, 50, 60 };
+            List<int> dates = new List<int>();
+
+            switch (dateselecter)
+            {
+                case 7:
+                    dates = Week;
+                    break;
+                case 12:
+                    dates = Month;
+                    break;
+                case 24:
+                    dates = Year;
+                    break;
+
+            }
+            foreach (var value in dates)
+            {
+
+                int i = Limitcheck(200, 0, value);
+                string color = ColorRep(value, 200, 0);
+                entriesS.Add(new ChartEntry(i) { Color = SKColor.Parse(color), Label = "maandag", ValueLabel = value.ToString() });
+            }
+            chart = new LineChart()
+            {
+                Entries = entriesS,
+                LineMode = LineMode.Straight,
+                LineSize = 8,
+                LabelTextSize = 50,
+                PointMode = PointMode.Circle,
+                PointSize = 25,
+                MaxValue = 200,
+                MinValue = 0,
+            };
+            //200
+            //0
+            return chart;
+        }
 
     }
 }
--- App/Project_Green/Views/ChartPage.xaml.cs
+++ App/Project_Green/Views/ChartPage.xaml.cs
@@ -14,117 +14,27 @@ namespace Project_Green.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ChartPage : ContentPage
     {
-     
-        List<ChartEntry> entriesH = new List<ChartEntry>();
-        List<ChartEntry> entriesT = new List<ChartEntry>();
-        List<ChartEntry> entriesS = new List<ChartEntry>();
+        
         StatMeister pieter = new StatMeister();
         public ChartPage()
         {
-            
+
             InitializeComponent();
             List<string> dates = new List<string> { "Week", "Month", "Year" };
             DateSelecter.ItemsSource = dates;
-            FillHumidityChart();
-            FillTempratureChart();
-            FillSoilMoisterChart();
-
+            fillCharts();
+        }
+        public void fillCharts()
+        {
+            int dateselecter = pieter.EntryCount(Convert.ToString(DateSelecter.SelectedItem));
+            HumidityChart.Chart = pieter.MakeLineChart(dateselecter);
+            TempratureChart.Chart = pieter.FillTempratureChart(dateselecter);
+            MoisterChart.Chart = pieter.FillSoilMoisterChart(dateselecter);
         }
        
-        private void FillHumidityChart()
+        private void DateChanged(object sender, EventArgs e)
         {
-
-            HumidityChart.Chart = new LineChart()
-            {
-                Entries = entriesH,
-                LineMode = LineMode.Straight,
-                LineSize = 8,
-                LabelTextSize = 50,
-                PointMode = PointMode.Circle,
-                PointSize = 25,
-                MaxValue = 100,
-                MinValue = 0
-            };
-
-            //maak methode aan om data vissen uit databas
-            List<int> rows = new List<int>();
-            foreach(var row in rows)
-            {
-                int x = 20;
-                int i = pieter.Limitcheck(100  ,0, x);
-                string color = pieter.ColorRep(x, 100, 0);
-                entriesH.Add(new ChartEntry(i) { Color = SKColor.Parse(color), Label = "maandag", ValueLabel = x.ToString() });
-            }
-            //max 100%
-            //min 0%
+            fillCharts();
         }
-        private void FillTempratureChart()
-        {
-            TempratureChart.Chart = new LineChart()
-            {
-                Entries = entriesT,
-                LineMode = LineMode.Straight,
-                LineSize = 8,
-                LabelTextSize = 50,
-                PointMode = PointMode.Circle,
-                PointSize = 25,
-                MaxValue = 50,
-                MinValue = -50
-            };
-            int x = 20;
-            int i = pieter.Limitcheck(50, -50, x);
-            string color = pieter.ColorRep(x, 50, -50);
-            entriesT.Add(new ChartEntry(i) { Color = SKColor.Parse(color), Label = "maandag", ValueLabel = x.ToString() });
-
-            //50 *C
-            //-50 *C
-        }
-        private void FillSoilMoisterChart()
-        {
-            MoisterChart.Chart = new LineChart()
-            {
-                Entries = entriesS,
-                LineMode = LineMode.Straight,
-                LineSize = 8,
-                LabelTextSize = 50,
-                PointMode = PointMode.Circle,
-                PointSize = 25,
-                MaxValue = 50,
-                MinValue = -50
-            };
-            int x = 20;
-            int i = pieter.Limitcheck(200, 0, x);
-            string color = pieter.ColorRep(x, 200, 0);   
-            entriesS.Add(new ChartEntry(i) { Color = SKColor.Parse(color), Label = "maandag", ValueLabel = x.ToString() });
-            //200
-            //0
-        }
-        private int EntryCount()
-        {
-            int AmountOfTimes =0;
-            string date = DateSelecter.SelectedItem.ToString();
-            Dictionary<string, int> TimeDates = new Dictionary<string, int>();
-            TimeDates.Add("Week", 7);
-            TimeDates.Add("Month", 12);
-            TimeDates.Add("Year", 24);
-
-            switch (date)
-            {
-                case "Week":
-                    AmountOfTimes = TimeDates["Week"];
-                    break;
-                case "Month":
-                    AmountOfTimes = TimeDates["Month"];
-                    break;
-                case "Year":
-                    AmountOfTimes = TimeDates["Year"];
-                    break;
-                default:
-                    AmountOfTimes = TimeDates["Week"];
-                    break;
-            }
-            return AmountOfTimes;
-        }
-       
     }
 }
