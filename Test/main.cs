c600b661d10334ea6fca80843b802de6b3ac8441
Edgar <edgar@anotherfoxguy.com>
2019-06-26 10:21:28 +0200

    :tada: Example code

--- Test/main.cs
+++ Test/main.cs
@@ -1,5 +1,8 @@
 using System;
-using ArdunoRest;
+using System.Collections.Generic;
+using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace CliTest
 {
@@ -7,12 +10,39 @@ namespace CliTest
     {
         public static int Main(String[] args)
         {
-            var rest = new ArdunoRestClient();
-            rest.BaseUrl = "http://192.168.1.2";
-            
-            Console.WriteLine(rest.Id().Name);
-            
+            var document = File.ReadAllText(@"TEST.TXT");
+            var input = new StringReader(document);
+
+            var deserializer = new DeserializerBuilder()
+                .Build();
+
+            var list = deserializer.Deserialize<List<Sensor>>(input);
+
+            Console.WriteLine("Order");
+            Console.WriteLine("-----");
+            Console.WriteLine();
+            foreach (var item in list)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}",
+                    item.temperature,
+                    item.humidity,
+                    item.lightlevel,
+                    item.moisture);
+            }
+
+
             return 0;
+        }
+
+        public class Sensor
+        {
+            public string time { get; set; }
+            public float temperature { get; set; }
+            public float humidity { get; set; }
+            public float lightlevel { get; set; }
+            public int moisture { get; set; }
+            
+            public int waterlevel { get; set; }
         }
     }
 }
