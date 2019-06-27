818b25b36625c4640365729eb8deb8eb16d62cdd
SUPERetnad01 <43534487+SUPERetnad01@users.noreply.github.com>
2019-06-27 20:14:51 +0200

    Documentatie :pencil: (#62)
    * Documentatie
    
    * kkk

--- App/Project_Green/Views/MainPage.xaml.cs
+++ App/Project_Green/Views/MainPage.xaml.cs
@@ -11,6 +11,9 @@ namespace Project_Green
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {  
+        /// <summary>
+        /// Constructor of MainPage
+        /// </summary>
         public MainPage()
         {
             InitializeComponent();
@@ -18,23 +21,33 @@ namespace Project_Green
             GreenhouseList.ItemsSource = IPScanner.Instance.GetGreenhouses();
             GreenhouseList.ItemsSource = DatabaseManager.Instance.GetGreenhouses(); // haal deze weg in uit in eind product
         }
-
+        /// <summary>
+        /// Scans for greenhouses 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ScanGreenhouses(object sender, EventArgs e)
         {
             GreenhouseList.ItemsSource = IPScanner.Instance.GetGreenhouses();
         }
-
+        /// <summary>
+        /// When Greenhouse is selected Go to settings
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void GreenhouseList_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             var SelectedGreenhouse = e.Item as Greenhouse;
             var cp = new ChartPage(SelectedGreenhouse);
             Navigation.PushAsync(cp);
         }
+        /// <summary>
+        /// When you come back to the page scan for green houses 
+        /// </summary>
         protected override void OnAppearing()
         {
             base.OnAppearing();
             GreenhouseList.ItemsSource = IPScanner.Instance.GetGreenhouses();
-            GreenhouseList.ItemsSource = DatabaseManager.Instance.GetGreenhouses(); //haal deze weg in eind product 
         }
     }
 }
